1b6f789a6bd0934f1486427cc561fcd3d2cdb1a2
Mohammad-h <784709@pdsb.net>
2023-08-19 16:13:23 -0400

    Make my own function to reverse an array

--- reverseArray.js
+++ reverseArray.js
@@ -1,3 +1,9 @@
 const arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr);
+function reverseArr(input){
+    let newArr = []
+    for (i = arr.length - 1; i >= 0; i--){
+        newArr.push(input[i]);
+    }
+    return newArr;
+}
+console.log(reverseArr(arr));
