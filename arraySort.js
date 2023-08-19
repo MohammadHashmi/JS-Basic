bb8cdc8e4472561154c15c7f773e9ea12c66a65f
Mohammad-h <784709@pdsb.net>
2023-08-19 16:25:09 -0400

    fixed comment

--- arraySort.js
+++ arraySort.js
@@ -1,4 +1,4 @@
 const arr = [5, 1, 24, 6, 71];
 arr.sort(function(a, b){return a - b})
 console.log(arr);
-https://www.w3schools.com/js/js_array_sort.asp
+//https://www.w3schools.com/js/js_array_sort.asp
