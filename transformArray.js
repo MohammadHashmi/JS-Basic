29e5c1976eef3757b748191a5629abab0bc35ab7
Mohammad-h <784709@pdsb.net>
2023-08-27 12:45:20 -0400

    Started problem 2635 on leetcode.

---
+++ transformArray.js
@@ -0,0 +1,10 @@
+function map(arr, fn) {
+    let newArr = [];
+    for (i = 0; i < arr.length; i++)
+    {
+        
+        newArr.push();
+    }
+}
+
+console.log(map([1,2,3]), function plusone(n) {return n + 1;});
